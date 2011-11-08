dee04f4887d58ffe0ba739b7fb0cdf8c0515dc4a
Przybylski 중광 <r351574nc3@gmail.com>
2011-11-08 06:49:30 -0700

    Fleshing out project information to get ready for site. Configuring site plugin

---
+++ src/main/java/org/kuali/rice/liquibase/change/ext/KimAddRoleChange.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.liquibase.change.ext;
+
+import liquibase.database.Database;
+
+import static liquibase.ext.Constants.EXTENSION_PRIORITY;
+
+/**
+ *
+ * @author Leo Przybylski
+ */
+public class KimCreateRoleChange extends AbstractChange {
+    
+
+    public KimAddRoleChange() {
+        super("KimAddRoleChange", "Adding a Role to KIM", EXTENSION_PRIORITY);
+    }
+    
+    /**
+     * Supports all databases 
+     */
+    @Override
+    public boolean supports(Database database) {
+        return true;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public ValidationErrors validate(Database database) {
+        return super.validate(database);
+    }
+
+    /**
+     * Generates the SQL statements required to run the change.
+     *
+     * @param database databasethe target {@link liquibase.database.Database} associated to this change's statements
+     * @return an array of {@link String}s with the statements
+     */
+    public SqlStatement[] generateStatements(Database database) {
+    }
+
+
+    /**
+     * Used for rollbacks. Defines the steps/{@link Change}s necessary to rollback.
+     * 
+     * @return {@link Array} of {@link Change} instances
+     */
+    protected Change[] createInverses() {
+        return null;
+    }
+
+}
