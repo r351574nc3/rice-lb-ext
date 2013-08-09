8831944cf34dfe10efc7fcefe65a226c2159f837
Dylan Viljoen <dylan.viljoen@nwu.ac.za>
2013-08-09 17:07:40 +0200

    Assign Role Tests

---
+++ src/test/java/org/kuali/rice/liquibase/change/ext/AssignRoleTests.java
@@ -0,0 +1,16 @@
+package org.kuali.rice.liquibase.change.ext;
+
+public class AssignRoleTests extends KimChangeBaseTest{
+
+	@Override
+	protected String entityName() {
+		return "krim_role_mbr_t";
+	}
+
+	@Override
+	protected String whereClause() {
+		final String roleId = "(select ROLE_ID from KRIM_ROLE_T where ROLE_NM = 'User' and NMSPC_CD = 'KUALI')";
+		final String memberId = "(select PRNCPL_ID from KRIM_PRNCPL_T where PRNCPL_NM = 'kr')";
+		return String.format("role_id in %s and MBR_ID in %s", roleId, memberId);
+	}
+}
